23a939964214d2648e7e7a7caf7d2fb438f5873e
McFuzzykins <77823726+McFuzzykins@users.noreply.github.com>
2021-02-15 11:41:16 -0800

    Add files via upload

---
+++ Scripts/EnemyBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Lifeform detected - EXTERMIATE!");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Lifeform out of range, find them!");
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
